07a069e3f3e556ebc53fa5f5bfc86eb8e586f224
Jose Paredes <jozemparedes@gmail.com>
2020-06-11 01:14:08 -0600

    initial commit

---
+++ src/containers/App.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+
+<style>
+    :global(body){
+        background-color: #676778;
+        color: white;
+    }
+
+
+</style>
+
+<div class="main">
+    <h1>Hello Petstagram</h1>
+
+</div>
