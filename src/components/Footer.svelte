6f51f1534e494ae8fce4d1edcaeb0e26a2c19dc5
Jose Paredes <jozemparedes@gmail.com>
2020-06-11 23:24:11 -0600

    like interaction

---
+++ src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<!-- Footer.svelte -->
+
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+
+<div class="Footer">
+    <div class="Footer-container">
+        <div class="Footer-copy">
+            <span>© 2020 PETSTAGRAM POWERED BY JOSE</span>
+        </div>
+    </div>
+</div>
---
+++ src/components/Profile.svelte
@@ -0,0 +1,45 @@
+<script>
+    export let nickname;
+    export let name;
+
+</script>
+
+<!-- Profile.svelte -->
+
+<style>
+  .Profile-content {
+    display: flex;
+    align-items: center;
+  }
+  .Profile-avatar img {
+    width: 42px;
+    height: 42px;
+    border-radius: 50%;
+  }
+  .Profile-info {
+    margin: 0 0 0 0.5em;
+  }
+  .Profile-info h2 {
+    font-size: 14px;
+    color: black;
+    margin: 0;
+    padding: 0;
+  }
+  .Profile-info span {
+    font-size: 12px;
+    font-weight: normal;
+  }
+</style>
+
+
+<div class="Profile">
+    <div class="Profile-content">
+        <div class="Profile-avatar">
+            <img src="https://picsum.photos/id/237/100" alt="" />           
+        </div>
+        <div class="Profile-info">
+            <h2>{nickname} </h2>
+            <span>{name} </span>
+        </div>
+    </div>
+</div>
